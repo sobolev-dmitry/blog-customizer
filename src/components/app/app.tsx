78a96f7115c8164f03862df5199594cb2c74a9ff
sobolev-dmitry <tuningstore96.ru@yandex.ru>
2026-05-28 14:47:47 +0500

    версия 1.0 протестировано и все работает

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,26 +1,30 @@
-import { CSSProperties } from 'react';
+import { useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from '../article/Article';
 import { ArticleParamsForm } from '../article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './../../constants/articleProps';
+import {
+	ArticleStateType,
+	defaultArticleState,
+} from '../../constants/articleProps';
 
 import styles from './app.module.scss';
 
 export const App = () => {
+	const [appSettings, setAppSettings] =
+		useState<ArticleStateType>(defaultArticleState);
+
+	const cssVariables = {
+		'--font-family': appSettings.fontFamilyOption.value,
+		'--font-size': appSettings.fontSizeOption.value,
+		'--font-color': appSettings.fontColor.value,
+		'--container-width': appSettings.contentWidth.value,
+		'--bg-color': appSettings.backgroundColor.value,
+	} as React.CSSProperties;
+
 	return (
-		<main
-			className={clsx(styles.main)}
-			style={
-				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
-				} as CSSProperties
-			}>
-			<ArticleParamsForm />
+		<main className={clsx(styles.main)} style={cssVariables}>
+			<ArticleParamsForm setConfig={setAppSettings} />
 			<Article />
 		</main>
 	);
